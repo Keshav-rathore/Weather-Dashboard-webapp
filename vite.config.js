c9e0ca5163e436e491f1606488896c63ac52730d
Keshav Singh Rathore <rathorekeshav989@gmail.com>
2025-08-16 11:26:38 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_WEATHER_API_KEY || "/Weather-Dashboard-webapp"
+  build: {
+    outDir: 'dist'
+  }
 })
